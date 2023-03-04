52e835d6e430412011993b97ba30273569c3d4b8
Z204544 <z204544@corp.clarobr>
2023-03-04 14:11:58 -0300

    add route component

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,1 +1,10 @@
-export {};
+import { Routes, Route, Navigate } from 'react-router-dom'
+
+export const AppRoutes = () => {
+    return (
+        <Routes>
+            <Route path='/pagina-inicial' element={<p>Páginal Inicial</p>}></Route>
+            <Route path='*' element={<Navigate to="/paginal-inicial"></Navigate>}></Route>
+        </Routes>
+    )
+}
